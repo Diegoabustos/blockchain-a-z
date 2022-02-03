ebcb622ca0a027880e8b2f90ef3d66c41ea27deb
Diego <diegoabustos24@gmail.com>
2022-02-02 19:49:36 -0600

    Obtener el ultimo bloque de la cadena

--- Modulo 1 - Crear una Cadena de  Bloques/blockchain.py
+++ Modulo 1 - Crear una Cadena de  Bloques/blockchain.py
@@ -31,5 +31,24 @@ class Blockchain:
                 'proof': proof,
                 'previous_hash': previous_hash
             }
+        self.chain.append(block)
+        return block
 
+    def get_previous_block(self):
+        return self.chain[-1]
 # Parte 2 - Minado de un BLoque de la Cadena
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
